10b3f8b08ffd42c67db0ad9288ed8700497d64bf
Leyla Shafiyeva <llshafi03@gmail.com>
2022-11-11 01:18:37 +0400

    Add project files.

---
+++ UMLHW1/Prototype/Models/ProfileSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLHW1.Prototype.Models;
+
+public class ProfileSettings
+{
+    public bool IsPrivate { get; set; }
+    public bool HideEmail { get; set; }
+    public bool HideAge { get; set; }
+
+    public ProfileSettings Clone()
+    {
+        return new ProfileSettings()
+        {
+            IsPrivate = IsPrivate,
+            HideEmail = HideEmail,
+            HideAge = HideAge
+        };
+    }
+}
---
+++ UMLHW1/Prototype/Models/ProfileSettingsPrototypeRegistry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLHW1.Prototype.Models
+{
+    public class ProfileSettingsPrototypeRegistry
+    {
+        private readonly ProfileSettings _defaultProfileSettings;
+        private readonly ProfileSettings _secureProfileSettings;
+
+        public ProfileSettingsPrototypeRegistry(ProfileSettings defaultProfileSettings, ProfileSettings secureProfileSettings)
+        {
+            _defaultProfileSettings = defaultProfileSettings;
+            _secureProfileSettings = secureProfileSettings;
+        }
+
+        public ProfileSettings CreateDefaultProfileSettings()
+        {
+            return _defaultProfileSettings.Clone();
+        }
+
+        public ProfileSettings CreateSecureProfileSettings()
+        {
+            return _secureProfileSettings.Clone();
+        }
+    }
+}
