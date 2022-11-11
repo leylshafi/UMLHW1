ad1d8bc4f0d74b09e9aeb2c8a74f661e82820152
Leyla Shafiyeva <llshafi03@gmail.com>
2022-11-11 11:42:40 +0400

    Add Iterator

---
+++ UMLHW1/Iterator/IteratorProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLHW1.Iterator
+{
+    internal class IteratorProgram
+    {
+    }
+}
